1fc78df70ec062d8a7590c47774f0dea7d1f9e2c
Defaultao <celiojr932ster@gmail.com>
2022-02-17 16:22:10 -0300

    Formulários de cadastro criado

--- src/pages/SingUpPage/SingUpPage.js
+++ src/pages/SingUpPage/SingUpPage.js
@@ -1,12 +1,95 @@
 import React from "react"
 import { Header } from "../../components/Header"
+import { Container, StyledTextField, StyledButton} from './styled'
+import useForm from '../../hooks/useForm'
+import logo from '../../assets/images/logo-future-eats-red.png'
+import { useNavigate } from "react-router-dom"
+import { goToAdressPage } from "../../routes/coordinator"
 
 const SingUpPage = () => {
+
+    const { form, onChangeForm, clearFields } = useForm({
+      name: '',
+      email: '',
+      cpf: '',
+      password:''
+    })
+  
+  const navigate = useNavigate()
+
   return (
-    <div>
-      <Header/>
-      <h1>SingUpPage</h1>
-    </div>
+    <Container>
+      {/* <Header/> */}
+      <img src={logo} alt="logo red" />
+      <h1>Cadastrar</h1>
+      <form>
+        <StyledTextField
+          id="outlined-basic"
+          name="name"
+          label="Nome"
+          placeholder="Nome e Sobrenome"
+          variant="outlined"
+          value={form.name}
+          onChange={onChangeForm}
+          type="text"
+          required
+        />
+        <StyledTextField
+          id="outlined-basic"
+          name="email"
+          label="E-mail"
+          placeholder="email@email.com"
+          variant="outlined"
+          value={form.email}
+          onChange={onChangeForm}
+          type="email"
+          required
+        />
+        <StyledTextField
+          id="outlined-basic"
+          name="cpf"
+          label="CPF"
+          placeholder="000.000.000-00"
+          variant="outlined"
+          value={form.cpf}
+          onChange={onChangeForm}
+          type="text"
+          required
+        />
+        <StyledTextField
+          id="outlined-basic"
+          name="password"
+          label="Senha"
+          placeholder="Mínimo 6 caracteres"
+          variant="outlined"
+          value={form.password}
+          onChange={onChangeForm}
+          type="password"
+          required
+        />
+        <StyledTextField
+          id="outlined-basic"
+          name="password"
+          label="Confirmar"
+          placeholder="Confirme a senha anterior"
+          variant="outlined"
+          value={form.password}
+          onChange={onChangeForm}
+          type="password"
+          required
+        />
+        <StyledButton onClickCapture={()=> goToAdressPage(navigate)}
+          textPrimary={'primary'}
+          color={'primary'}
+          fullWidth
+          variant="contained"
+        >
+          Criar
+        </StyledButton>
+        </form>
+
+      
+    </Container>
   )
 }
 
