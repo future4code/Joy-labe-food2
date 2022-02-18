178a7265d58a19e241ab76d14cf05855fdceb4d6
Matheus Lima Ribeiro <matheuslimaribeiro15@outlook.com>
2022-02-17 21:45:26 -0300

    fiz o useRequestData

---
+++ src/hooks/useRequestData.js
@@ -0,0 +1,30 @@
+import axios from "axios"
+import { BASE_URL } from "../constants/urls"
+import { useEffect, useState } from "react"
+
+const useRequestData = (initialData, path) => {
+  const [data, setData] = useState(initialData)
+
+  const headers = {
+    headers: {
+      auth: localStorage.getItem("token"),
+    },
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      axios
+        .get(`${BASE_URL} + ${path}`, headers)
+        .then(({ data }) => {
+          setData(data)
+        })
+        .catch((err) => {
+          console.log(err.data.response.message)
+        })
+    }
+  }, [path])
+
+  return { data }
+}
+
+export default useRequestData
