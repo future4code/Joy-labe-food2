3b378611acbbfb54ef5d3e9cd52e8572fbb39918
Guilherme de Oliveira Barros <47544503+FIXER3600@users.noreply.github.com>
2022-03-21 19:32:07 -0300

    Revert "PR de Correção"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { ThemeProvider } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-import theme from './constants/theme'
-import AppRoutes from './routes/AppRoutes'
-
-const App = () => {
+function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <AppRoutes />
-    </ThemeProvider>
-  )
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
